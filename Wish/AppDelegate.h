28963415eb0b301da834421afa0c655dc2fa4eba
Annie Klekchyan <annie.klekchyan@gmail.com>
2016-01-15 12:40:47 +0400

    Initial Commit

---
+++ Wish/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Wish
+//
+//  Created by Annie Klekchyan on 1/15/16.
+//  Copyright © 2016 TLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Wish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wish
+//
+//  Created by Annie Klekchyan on 1/15/16.
+//  Copyright © 2016 TLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
